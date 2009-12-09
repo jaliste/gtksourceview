03216cbb14b3a1e903e15835a2aa806121a649e9
Jose Aliste <jose.aliste@gmail.com>
2009-12-09 01:02:57 +0100

    Rendering with the gutter API

--- gtksourceview/gtksourcefold.h
+++ gtksourceview/gtksourcefold.h
@@ -46,6 +46,11 @@ GtkSourceFold		*gtk_source_fold_get_parent	(GtkSourceFold       *fold);
 
 const GList		*gtk_source_fold_get_children	(GtkSourceFold       *fold);
 
+void 			gtk_source_fold_get_lines	(GtkSourceFold 	     *fold,
+							 GtkTextBuffer 	     *buffer, 
+							 gint 		     *start_line,
+			 				 gint 		     *end_line); 
+
 G_END_DECLS
 
 #endif  /* __GTK_SOURCE_FOLD_H__ */
