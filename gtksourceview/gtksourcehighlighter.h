1105c8a778ceabdc9b2c97fd7f679f7d4f152f40
Jose Aliste <jaliste@src.gnome.org>
2010-06-17 19:43:10 -0400

    Add refresh_region and ensure_higlight back to the Highlighter. Delete refresh_region from the Engine.

--- gtksourceview/gtksourcehighlighter.h
+++ gtksourceview/gtksourcehighlighter.h
@@ -59,6 +59,10 @@ void 			_gtk_source_highlighter_set_styles_map	 (GtkSourceHighlighter *highlight
 				       				  GHashTable 	       *styles);
 void			_gtk_source_highlighter_set_analyzer	 (GtkSourceHighlighter *highlighter,
 								  GtkSourceEngine      *se);
+void			_gtk_source_highlighter_ensure_highlight (GtkSourceHighlighter *highlighter,
+		    						  const GtkTextIter      *start,
+		    						  const GtkTextIter      *end);
+
 G_END_DECLS
 
 #endif /* __GTK_SOURCE_HIGHLIGHTER_H__ */
